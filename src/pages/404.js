30acacd9ea133617821380edb949baaad84d483b
binamralamsal <binamralamsal@gmail.com>
2023-04-08 23:13:36 +0545

    Not found page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+/*
+ * Next.js by default includes a 404 page for you.
+ * but you should always replace it with your own for giving it
+ * your own look and feel matching your website.
+ */
+export default function NotFoundPage() {
+  return <div>404 error occurred!</div>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,6 @@
-import { useRouter } from "next/router";
-
 export default function Home(props) {
-  const router = useRouter();
-
-  function handleOpenTweets() {
-    // This will redirect users to /tweets/
-    router.push("/tweets");
-
-    /*
-     * router.replace is simlar to router.push, but it will
-     * prevent adding a new URL entry to history stack.
-     * But, what does that mean? Use router.push() then click on that button
-     * then you will realize that you can press back button of your browser
-     * but instead if you use router.replace() then you will realize that
-     * you won't go back to previous page as router.replace() replaces your history stack.
-     *
-     * router.replace("/tweets");
-     */
-
-    /*
-     * As expected, it will redirect back to previous page.
-     *
-     * router.back();
-     */
-
-    /*
-     * As expected, it will reload the page
-     *
-     * router.reload();
-     */
-  }
   return (
     <>
-      {/*
-       * Although, we are redirecting users via button to showcase in this example
-       * please never ever do this. Always use link tag for this.
-       */}
-      <button onClick={handleOpenTweets}>Open Tweets</button>
       {/* You can then gain access to this secret message using props because that's what you passed through _app.js */}
       <p>{props.message}</p>
       <div>Page Content</div>
