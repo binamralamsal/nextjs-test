626bdba19bc51c43127ba753570da237c8572def
binamralamsal <binamralamsal@gmail.com>
2023-04-08 22:34:37 +0545

    Dynamic routes folder way

---
+++ src/pages/tweets/[id]/edit.js
@@ -0,0 +1,5 @@
+// This page will be mapped to /tweets/1/edit, /tweets/2/edit, etc.
+
+export default function EditTweetPage() {
+  return <div>Edit Tweet Page</div>;
+}
---
+++ src/pages/tweets/index.js
@@ -0,0 +1,5 @@
+// This page will be mapped to /tweets
+
+export default function ExploreTweetsPage() {
+  return <div>Explore Tweets Page</div>;
+}
