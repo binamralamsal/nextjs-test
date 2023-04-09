3a4117eab03e7c81c7e7286b1b0fbf3ab6c9fae5
binamralamsal <binamralamsal@gmail.com>
2023-04-09 20:21:12 +0545

    getStaticPaths; fetching paths from API

--- src/pages/tweets/[id]/index.js
+++ src/pages/tweets/[id]/index.js
@@ -1,18 +1,4 @@
-import { useRouter } from "next/router";
-
 export default function SingleTweetPage(props) {
-  const router = useRouter();
-
-  /*
-   * When fallback passed from getStaticPaths is set to true, Next.js will serve a "fallback" version.
-   * When router.isFallback is true, props.post will be undefined. So, we need to check if fallback
-   * version is being served or not. If it is, we will show loading indicator. Otherwise, we will show
-   * the page.
-   *
-   * It's not needed when you are using fallback: "blocking"
-   */
-  if (router.isFallback) return <div>Loading</div>;
-
   console.log(props.post);
 
   return (
@@ -24,43 +10,27 @@ export default function SingleTweetPage(props) {
 }
 
 export async function getStaticPaths() {
-  const paths = [
-    { params: { id: "1" } },
-    { params: { id: "2" } },
-    { params: { id: "3" } },
-    { params: { id: "4" } },
-    { params: { id: "5" } },
-  ];
-
   /*
-   * We discussed, how we can specify paths for getStaticProps. But, what if we have
-   * 1000s of posts and we don't want to specify all of them? We can specify paths for
-   * popular posts then allow Next.js to generate pages for other posts on demand.
+   * We learned how we can specify different paths for our pages. But what if our data is stored in
+   * database or different locations? Well, since it runs only in server, you can run any server
+   * side code which means you can retrieve data from an API or database then return different
+   * paths that you want. You can either return all paths or just return a few paths with fallback
+   * set to "blocking" or true.
    *
-   * fallback value accepts two values, boolean or "blocking". Default behavior is false.
-   * By default or when fallback is false, any paths not returned by getStaticPaths will result
-   * in 404 error. This means, /tweets/6 will result in 404 error because it's not specified inside
-   * paths.
-   *
-   * When fallback is set to true, Next.js will serve a "fallback" version fo the page on first request.
-   * This fallback version is just a blank page with loading indicator. Then, Next.js will generate
-   * the full version of the page in the background and serve it to the user. This means, /tweets/6
-   * will result in a blank page with loading indicator, and then it will show the page.
-   * Next.js will just run getStaticProps in background to get data for the specified id in background.
-   * This data will be cached for future use, which means if you visit /tweets/6 again, you won't see
-   * loading indicator. Build the project to see this in action.
-   *
-   * When fallback is set to "blocking", Next.js will wait for getStaticProps to finish before
-   * serving the page.
+   * Alternative way is to return empty paths with fallback set to blocking. This way, no pages will
+   * be generated during build time but when a user visits a page, Next.js will generate the page
+   * on demand and cache for future use, which means if user visits the same page again, it will
+   * be served from cache.
    */
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  const posts = await res.json();
+  const paths = posts.map((p) => ({
+    params: { id: p.id.toString() },
+  })); // This is just generating multiple paths from posts
+
   return { paths, fallback: "blocking" };
-
-  /*
-   * When path is set to empty [] with fallback: "blocking", this page will essentially behave similar to
-   * getServerSideProps which we will discuss later. The only difference is that, once it has generated page
-   * for a specific path, it will cache and reuse that later on which is not the case with getServerSideProps.
-   */
-  // return { paths: [], fallback: "blocking" };
 }
 
 export async function getStaticProps(context) {
